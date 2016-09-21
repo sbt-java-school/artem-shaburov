2fbc9b14e1c1efeb75e588308f40d898924875d7
Artem <kiski4ever@gmail.com>
2016-09-21 11:30:48 +0700

    streams tests

---
+++ src/test/java/homework11/Person2.java
@@ -0,0 +1,29 @@
+package homework11;
+
+/**
+ * @author artem
+ */
+public class Person2 {
+    private String name;
+    private int age;
+
+    public Person2(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
