44fb7cc11ce132003dfe5d8617f4a2dff623605e
Artem <kiski4ever@gmail.com>
2016-09-21 11:24:54 +0700

    added serialization for truck

--- src/main/java/homework4/Main.java
+++ src/main/java/homework4/Main.java
@@ -1,5 +1,6 @@
 package homework4;
 
+import java.io.*;
 import java.util.*;
 
 /**
@@ -19,28 +20,53 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String fileName = "data.ser";
+
+        Truck truck = new Truck(10, Truck.Type.KAMAZ, 100);
+        serialize(fileName, truck);
+        System.out.println(truck);
+
+        Truck truck2 = deserialize(fileName);
+        System.out.println(truck2);
+
+//        // старый код
 //        TruckDao truckDao = new TruckDaoMemoryImpl();
 //        Main application = new Main(truckDao);
 //        application.viewTruckRegistry();
+//
+//        // 1. просто мультимап с известными типами
+//        TruckMultimap truckMultimap = new TruckMultimapImpl();
+//        truckMultimap.put(Truck.Type.KAMAZ, new Truck(1, Truck.Type.KAMAZ, 100));
+//        truckMultimap.put(Truck.Type.VOLVO, new Truck(2, Truck.Type.VOLVO, 100));
+//        List<Truck> kamazList = truckMultimap.get(Truck.Type.KAMAZ);
+//        System.out.println(kamazList);
+//        Map<Truck.Type, List<Truck>> truckRegistryByType = ((TruckMultimapImpl) truckMultimap).getTruckRegistryByType();
+//        System.out.println(truckRegistryByType);
+//
+//        // 2. мультимап с дженериками
+//        GenericMultimap<Truck.Type, Truck> genericMultimap = new GenericMultimapImpl<>();
+//        genericMultimap.put(Truck.Type.VOLVO, new Truck(1, Truck.Type.VOLVO, 100));
+//        genericMultimap.put(Truck.Type.KAMAZ, new Truck(1, Truck.Type.KAMAZ, 100));
+//        List<Truck> volvoList = genericMultimap.get(Truck.Type.VOLVO);
+//        System.out.println(volvoList);
+//        Map multimap = ((GenericMultimapImpl) genericMultimap).getMultimap();
+//        System.out.println(multimap);
+    }
 
-        // 1. просто мультимап с известными типами
-        TruckMultimap truckMultimap = new TruckMultimapImpl();
-        truckMultimap.put(Truck.Type.KAMAZ, new Truck(1, Truck.Type.KAMAZ, 100));
-        truckMultimap.put(Truck.Type.VOLVO, new Truck(2, Truck.Type.VOLVO, 100));
-        List<Truck> kamazList = truckMultimap.get(Truck.Type.KAMAZ);
-        System.out.println(kamazList);
-        Map<Truck.Type, List<Truck>> truckRegistryByType = ((TruckMultimapImpl) truckMultimap).getTruckRegistryByType();
-        System.out.println(truckRegistryByType);
+    private static <T> T deserialize(String fileName) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Object object = ois.readObject();
+        ois.close();
+        return  (T) object;
+    }
 
-        // 2. мультимап с дженериками
-        GenericMultimap<Truck.Type, Truck> genericMultimap = new GenericMultimapImpl<>();
-        genericMultimap.put(Truck.Type.VOLVO, new Truck(1, Truck.Type.VOLVO, 100));
-        genericMultimap.put(Truck.Type.KAMAZ, new Truck(1, Truck.Type.KAMAZ, 100));
-        List<Truck> volvoList = genericMultimap.get(Truck.Type.VOLVO);
-        System.out.println(volvoList);
-        Map multimap = ((GenericMultimapImpl) genericMultimap).getMultimap();
-        System.out.println(multimap);
+    private static void serialize(String fileName, Object object) throws IOException {
+        FileOutputStream fos = new FileOutputStream(fileName);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(object);
+        oos.close();
     }
 
     private void viewTruckRegistry() {
--- src/main/java/homework4/Truck.java
+++ src/main/java/homework4/Truck.java
@@ -1,16 +1,20 @@
 package homework4;
 
+import java.io.*;
+
 /**
  * Created by art
  */
-public class Truck {
-    enum Type {
-        MAN, VOLVO, KAMAZ
-    }
+public class Truck implements Serializable {
+    private static final long serialVersionUID = 4893084921904046779L;
 
     private long id;
     private Type type;
     private int capacity;
+
+    public enum Type {
+        MAN, VOLVO, KAMAZ
+    }
 
     public Truck(long id, Type type, int capacity) {
         this.id = id;
@@ -58,5 +62,45 @@ public class Truck {
                 ", type=" + type +
                 ", capacity=" + capacity +
                 '}';
+    }
+
+    /* homework 10 */
+    private void readObject(ObjectInputStream inputStream) throws IOException, ClassNotFoundException {
+        inputStream.defaultReadObject();
+        id = inputStream.readLong();
+        type = (Type) inputStream.readObject();
+        capacity = inputStream.readInt();
+    }
+
+    private void writeObject(ObjectOutputStream outputStream) throws IOException {
+        outputStream.defaultWriteObject();
+        outputStream.writeLong(id);
+        outputStream.writeObject(type);
+        outputStream.writeInt(capacity);
+    }
+
+    private Object writeReplace() throws ObjectStreamException {
+        return new TruckProxy(this);
+    }
+}
+
+class TruckProxy implements Serializable {
+    private static final long serialVersionUID = 9111838873960553836L;
+    private String data;
+    transient private static final String DELIMITER = ", ";
+
+    public TruckProxy(Truck truck) {
+        data = truck.getId() + DELIMITER + truck.getType() + DELIMITER + truck.getCapacity();
+        System.out.println(data);
+    }
+
+    private Object readResolve() {
+        String[] pieces = data.split(DELIMITER); // когда встречаем последовательные два символа зпт + пробел
+        if (pieces.length != 3) {
+            throw new IllegalArgumentException("data not valid");
+        }
+        // если неправильный аргумент передать в парсинг инума, то будет illegalArgument, с интами и лонгами numberFormat
+        Truck truck = new Truck(Long.parseLong(pieces[0]), Truck.Type.valueOf(pieces[1]), Integer.parseInt(pieces[2]));
+        return truck;
     }
 }
